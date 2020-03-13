bd40f1e366967fd41ff8b0747f3466cad885256d
AtesArkuran <ates.arkuran@gmail.com>
2020-03-13 23:21:13 +0300

    Doing Stuff

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,35 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    tcpServer = new QTcpServer(this);
+
+    if (!tcpServer->listen(QHostAddress::Any,quint16(ui->lineEdit->text().toInt()))) {
+        QMessageBox::critical(this, tr("Chat Server"),
+                              tr("Unable to start the server: %1.")
+                              .arg(tcpServer->errorString()));
+        close();
+        return;
+    }
+
+    qDebug() << tcpServer->serverPort();
+
+//! [0]
+    QString ipAddress;
+    QList<QHostAddress> ipAddressesList = QNetworkInterface::allAddresses();
+    // use the first non-localhost IPv4 address
+    for (int i = 0; i < ipAddressesList.size(); ++i) {
+        if (ipAddressesList.at(i) != QHostAddress::LocalHost &&
+            ipAddressesList.at(i).toIPv4Address()) {
+            ipAddress = ipAddressesList.at(i).toString();
+            break;
+        }
+    }
+    // if we did not find one, use IPv4 localhost
+    if (ipAddress.isEmpty())
+        ipAddress = QHostAddress(QHostAddress::LocalHost).toString();
+
+    ui->label_2->setText(ipAddress + " adresinde " + QString::number(tcpServer->serverPort()) + " portu açılmıştır.");
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,18 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <iostream>
+
 #include <QMainWindow>
+#include <QMessageBox>
+
+#include <QString>
+#include <QVector>
+#include <QDebug>
+
+#include <QtNetwork>
+#include <QNetworkSession>
+#include <QTcpServer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +26,12 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QTcpServer *tcpServer = nullptr;
+    QNetworkSession *networkSession = nullptr;
 };
 #endif // MAINWINDOW_H
